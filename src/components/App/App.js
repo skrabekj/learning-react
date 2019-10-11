3237c157f168bd86143bb48912d16044567e8d55
Jan Skrabek <jan.skrabek@tutanota.com>
2019-10-11 20:42:13 +0200

    first change

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <main className="app-content">
         <h1>My first React app</h1>
-      </div>
+        <h2>Hello world!</h2>
+      </main>
     )
   }
 }
