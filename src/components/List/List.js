8a38150dc0d55f06ecd853b0a0a4c9043b92d6ef
Jan Skrabek <jan.skrabek@tutanota.com>
2019-10-15 20:09:18 +0200

    updated App.js and List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,15 +3,18 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
+
 
 class List extends React.Component {
 
   static propTypes = {
   title: PropTypes.node,
-  children: PropTypes.node,
+  description: PropTypes.node,
+  columns: PropTypes.array,
   }
   static defaultProps = {
-  children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
   }
 
   render() {
@@ -19,7 +22,7 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} heroImage={this.props.image} />
         <div className={styles.description}>
-          {this.props.children}
+          {this.props.description}
         </div>
         <div className={styles.columns}>
           <Column title='animal' />
