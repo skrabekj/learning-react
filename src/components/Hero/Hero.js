66ca286eb1dceb57a72cd75f1fe9b86cf0168d79
Jan Skrabek <jan.skrabek@tutanota.com>
2019-10-15 20:25:46 +0200

    added ReactFtmlParser compatibility

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './Hero.scss';
 import PropTypes from 'prop-types';
-
+import ReactHtmlParser from 'react-html-parser';
 
 const Hero = props => {
   return <header className={styles.component}>
-    <h2 className={styles.title}>{props.titleText}</h2>
+    <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
     <img src={props.heroImage.src} alt={props.heroImage.alt} className={styles.image} />
   </header> }
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,7 +4,7 @@ import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
-
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
 
@@ -22,7 +22,7 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} heroImage={this.props.image} />
         <div className={styles.description}>
-          {this.props.description}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
           <Column title='animal' />
