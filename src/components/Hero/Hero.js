b7acf720f4e22171cf3a24e6ed4bc5de326def5a
Jan Skrabek <jan.skrabek@tutanota.com>
2019-10-15 21:24:48 +0200

    bug fixes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,13 +4,17 @@ import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
 
 const Hero = props => {
+  console.log('phc', props.heroImage.src);
+  console.log('phc', props.heroImage.alt);
+  console.log('img', props.heroImage);
   return <header className={styles.component}>
     <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
-    <img src={props.heroImage.src} alt={props.heroImage.alt} className={styles.image} />
+    <img src={props.heroImage} className={styles.image} />
   </header> }
 
 Hero.propTypes = {
 titleText: PropTypes.node,
-heroImage: PropTypes.shape({ src: PropTypes.string, alt: PropTypes.string })
+//heroImage: PropTypes.shape({ src: PropTypes.string })
+
 }
 export default Hero;
