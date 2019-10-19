84c7098ef07d74b944129684d1a7c63cf3b475c7
Jan Skrabek <jan.skrabek@tutanota.com>
2019-10-19 19:35:53 +0200

    redux store configuration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('app'));
-
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
