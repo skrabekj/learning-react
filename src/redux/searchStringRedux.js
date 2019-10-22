19930216cb642f0d4aa6e9294e97e1085b4ec298
Jan Skrabek <jan.skrabek@tutanota.com>
2019-10-22 20:38:51 +0200

    updated searchStringRedux

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -3,14 +3,18 @@ export const getSearchString = state => state.searchString;
 export const countAllCards = ({cards}) => cards.length;
 export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 // action name creator
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
-
+export const CHANGE = createActionName('CHANGE');
 // action creators
-
+export const createAction_changeSearchString = payload => ({ payload, type: CHANGE });
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return action.payload;
     default:
       return statePart;
   }
