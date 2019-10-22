9a2f5c73fdfa2f8b23440fdab193e6061b7bdb13
Jan Skrabek <jan.skrabek@tutanota.com>
2019-10-22 19:45:06 +0200

    added new search component files

---
+++ src/redux/searchStringRedux.js
@@ -0,0 +1,16 @@
+// selectors
+
+// action name creator
+
+// actions types
+
+// action creators
+
+// reducer
+export default function reducer(statePart = '', action = {}) {
+  switch (action.type) {
+    default:
+      return statePart;
+  }
+}
+
