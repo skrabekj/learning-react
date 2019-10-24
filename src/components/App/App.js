60eb39a521d907ad32fbb9fb201cf76e53e90ed7
Jan Skrabek <jan.skrabek@tutanota.com>
2019-10-24 19:07:40 +0200

    added dnd plugin and droppable component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Creator from '../Creator/Creator.js';
 import listStyle from '../List/List.scss';
 import PropTypes from 'prop-types';
 import Search from '../Search/SearchContainer.js';
+import {DragDropContext} from 'react-beautiful-dnd';
 
 class App extends React.Component {
   static propTypes = {
@@ -18,14 +19,19 @@ class App extends React.Component {
   }
   render() {
     const {title, subtitle, lists, addList} = this.props;
+    const moveCardHandler = result => {
+      console.log(result);
+    };
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
         <Search />
-        {lists.map(listData => (
-          <List key={listData.id} {...listData} />
-        ))}
+        <DragDropContext onDragEnd={moveCardHandler}>
+          {lists.map(listData => (
+            <List key={listData.id} {...listData} />
+          ))}
+        </DragDropContext>
         <div className={listStyle.creator}>
           <Creator text={'dodaj liste'} action={title => addList(title)}/>
         </div>
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -5,6 +5,7 @@ import Card from '../Card/Card.js';
 import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 import PropTypes from 'prop-types';
+import {Droppable} from 'react-beautiful-dnd';
 
 class Column extends React.Component {
 
@@ -12,7 +13,7 @@ class Column extends React.Component {
     icon: settings.defaultColumnIcon,
   }
   render() {
-    const {cards, addcard} = this.props; //icon, title,
+    const {cards, addcard, id} = this.props; //icon, title,
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
@@ -21,11 +22,19 @@ class Column extends React.Component {
           </span>
           {this.props.title}
         </h3>
-        <div className={styles.card}>
-          {cards.map(cardData => (
-            <Card key={cardData.id} {...cardData} />
-          ))}
-        </div>
+        <Droppable droppableId={id}>
+          {provided => (
+            <div className={styles.card}
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {cards.map(cardData => (
+                <Card key={cardData.id} {...cardData} />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
         <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={addcard}/>
         </div>
@@ -39,6 +48,7 @@ Column.propTypes = {
   icon: PropTypes.node,
   title: PropTypes.string,
   addcard: PropTypes.func,
+  id: PropTypes.string,
 };
 
 export default Column;
