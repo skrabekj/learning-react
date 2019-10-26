a710e44ac021e78dfccce0511d3f3e34501279b7
Jan Skrabek <jan.skrabek@tutanota.com>
2019-10-26 20:15:03 +0200

    added info component and routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
+import Info from '../Info/Info';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 const App = () => (
-  <div>
-    <Home />
-  </div>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/info' component={Info} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
